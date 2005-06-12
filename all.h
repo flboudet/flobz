e83c155a18c4eaebb0b96fa63b5fefb10e826582
gyom <gyom@053fdb51-1f17-0410-be66-d00040120cd6>
2005-06-12 23:03:17 +0000

    Added CF framework for mac...
    git-svn-id: svn+ssh://src.fovea.cc/svn/flobopuyo/trunk@380 053fdb51-1f17-0410-be66-d00040120cd6

--- all.h
+++ all.h
@@ -1,7 +1,12 @@
 #ifndef _ALL_H
 #define _ALL_H
 
+#if defined(__APPLE__)
+#include <CoreFoundation/CoreFoundation.h>
+#endif
+
 #include "PuyoCommander.h"
 #include "gametools/SDL_Painter.h"
+
 
 #endif
