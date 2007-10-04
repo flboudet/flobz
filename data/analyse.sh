6c57738591d0b1b05ebf814aa34ab96bf0a8af61
gyom <gyom@053fdb51-1f17-0410-be66-d00040120cd6>
2007-10-04 15:20:06 +0000

    Ajout auto des Makefiles qui devraient etre dans le svn.
    git-svn-id: svn+ssh://src.fovea.cc/svn/flobopuyo/trunk@722 053fdb51-1f17-0410-be66-d00040120cd6

--- data/analyse.sh
+++ data/analyse.sh
@@ -2,17 +2,48 @@
 
 tmpfile=tmp
 
+# Warning...
 echo "Ce script cree les Makefiles de tous les dossiers contenus dans 'data'."
 echo "Tous les fichiers et dossiers doivent etre commites au prealable."
+echo "Ce script ne gere pas les chemins contenant des espaces."
 
+# Recuperation des fichiers sur le svn
 svn -R ls > ${tmpfile}
 
+# Extraction des Makefiles
 MAKEFILES=$(grep Makefile ${tmpfile} | grep "/")
 
+# Verification qu'il existe un Makefiles par dossier
+FOLDERSTOCHECK=$(grep -E '^.*/$' ${tmpfile}  | grep -v Makefile | sort -u)
+for i in ${FOLDERSTOCHECK}
+do
+  found=0
+  k="${i%/*}/Makefile"
+  for j in ${MAKEFILES}
+  do
+    if [ "${k}" == "${j}" ]
+    then
+      found=1
+    fi
+  done
+    if [ ! ${found} -eq 1 ]
+    then
+      echo "WARNING: Le dossier ${i} dans le svn ne contient pas de Makefile."
+      echo "Creation et ajout dans le svn."
+      touch "$k"
+      svn add ${k}
+      MAKEFILES="${MAKEFILES} ${k}"
+    fi
+done
+
+exit 0
+
+# Pour chaque makefile
 for i in ${MAKEFILES}
 do
   echo "Updating $i"
   
+  # Ajouter les fichiers
   FILES=$(grep -E '^'"${i%Makefile}"'[^/]+$' ${tmpfile}  | grep -v Makefile | sort)
   FI="FILES="
   for j in ${FILES}
@@ -21,6 +52,7 @@ do
   done
   echo "${FI}" > ${i}
 
+  # Ajouter les dossiers
   FOLDERS=$(grep -E '^'"${i%Makefile}"'[^/]+/$' ${tmpfile}  | grep -v Makefile | sort)
   FO="DIRECTORIES="
   for j in ${FOLDERS}
@@ -30,8 +62,13 @@ do
   done
   echo "${FO}" >> ${i}
 
+  # Inserer les regles
   echo 'include ${SOURCE_DATADIR}/Datarules.mk' >> ${i}
-    
+
 done
 
+# Nettoyage
 rm -f tmp
+
+# Commit
+svn commit -m "Mise a jour des Makefiles des donnees" ${MAKEFILES}
