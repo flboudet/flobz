6d66b097ef756b357a50d8aa70684b967b1ee646
gyom <gyom@053fdb51-1f17-0410-be66-d00040120cd6>
2008-02-01 00:20:12 +0000

    Nettoyage de warnings.
    git-svn-id: svn+ssh://src.fovea.cc/svn/flobopuyo/trunk@867 053fdb51-1f17-0410-be66-d00040120cd6

---
+++ PuyoStrings.cpp
@@ -0,0 +1,89 @@
+const char *kYouDidIt     = "You Dit It!!!";
+const char *kNextLevel    = "Next:\t\t";
+const char *kLooser       = "Looser!!!";
+const char *kCurrentLevel = "Level:\t\t";
+const char *kContinueLeft = "Continues:\t\t";
+const char *kGameOver     = "Game Over!!!";
+const char *kYouGotToLevel = "You Get To ";
+const char *kHitActionToContinue = "Hit Action to continue...";
+const char *kHitActionForMenu = "Hit Action for menu...";
+const char *kContinue     = "Continue?";
+const char *kContinueGame = "Continue Game";
+const char *kAbortGame    = "Abort Game";
+const char *kOptions      = "Options";
+const char *kPlayer       = "Player";
+const char *kScore        = "Score:\t";
+const char *kPlayerName   = "Player Name:\t";
+const char *kPlayer1Name  = "P1 Name:\t";
+const char *kPlayer2Name  = "P2 Name:\t";
+const char *kIpAddress    = "IP Address: ";
+const char *kExit         = "Exit";
+
+
+const char *k01 = " 1 - ";
+const char *k02 = " 2 - ";
+const char *k03 = " 3 - ";
+const char *k04 = " 4 - ";
+const char *k05 = " 5 - ";
+const char *k06 = " 6 - ";
+const char *k07 = " 7 - ";
+const char *k08 = " 8 - ";
+const char *k09 = " 9 - ";
+const char *k10 = "10 - ";
+
+const char *kMustRestart1 = "Major Video Mode Change...";
+const char *kMustRestart2 = "This will take effect next time you restart Flobo Puyo.";
+const char *kHighScores  = "Hall of Fame";
+
+const char *kMusicVolume = "MusicVolume";
+const char *kAudioVolume = "AudioVolume";
+
+const char *kCongratulations = "Congratulations!!!";
+const char *kPuyosInvasion   = "                        You stopped Puyo's invasion.\n"
+"                         Peace on Earth is restored!!";
+
+const char *kAudioFX     = "Audio FX\t";
+const char *kMusic       = "Music\t";
+const char *kFullScreen  = "FullScreen\t";
+const char *kOpenGL      = "Use OpenGL\t";
+const char *kChangePuyoTheme = "Puyo theme...";
+const char *kChangePuyoLevel = "Level theme...";
+const char *kControls    = "Change controls...";
+const char *kGameLevel   = "Choose Game Level";
+const char *kLevelEasy   = "Easy";
+const char *kLevelMedium = "Medium";
+const char *kLevelHard   = "Hard";
+
+const char *kPlayer1Left  = "P1 Left:\t";
+const char *kPlayer1Right = "P1 Right:\t";
+const char *kPlayer1Down  = "P1 Down:\t";
+const char *kPlayer1Clockwise        = "P1 Turn R:\t";
+const char *kPlayer1Counterclockwise = "P1 Turn L:\t";
+
+const char *kPlayer2Left   = "P2 Left:\t";
+const char *kPlayer2Right  = "P2 Right:\t";
+const char *kPlayer2Down   = "P2 Down:\t";
+const char *kPlayer2Clockwise        = "P2 Turn R:\t";
+const char *kPlayer2Counterclockwise = "P2 Turn L:\t";
+
+const char *kRules01 = "Puyos are fancy smiling bubbles...";
+const char *kRules02 = "But they can really be invading sometimes!";
+const char *kRules03 = "Send them away by making groups of 4 or more.";
+const char *kRules04 = "Try to make large groups, or many at the same time";
+const char *kRules05 = "to send more bad ghost Puyos to your opponent.";
+
+const char *kAbout01 = "FloboPuyo is an iOS-software production.\t\t";
+const char *kAbout02 = "Puyo's world Meta God:\t\t";
+const char *kAbout03 = "\t\tFlorent 'flobo' Boudet";
+const char *kAbout04 = "X Men:\t\t";
+const char *kAbout05 = "\t\tJean-Christophe 'jeko' Hoelt";
+const char *kAbout06 = "\t\tGuillaume 'gyom' Borios";
+const char *kAbout07 = "Beta Goddess:\t\t";
+const char *kAbout08 = "\t\tTania";
+const char *kSinglePlayerGame = "Single Player Game";
+const char *kTwoPlayersGame = "Two Players Game";
+const char *kNetGame = "Network Game";
+
+const char *AI_NAMES[] = { "Fanzy", "Garou", "Big Rabbit", "Gizmo",
+  "Satanas", "Doctor X", "Tanya", "Mr Gyom",
+"The Duke","Jeko","--------" };
