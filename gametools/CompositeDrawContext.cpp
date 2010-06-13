f5ff5be8dc7986f4c17c72ac5d2611348ff69fe0
flobo <flboudet@gmail.com>
2010-06-13 00:02:36 +0000

    Premiere version du CompositeDrawContext
    git-svn-id: svn+ssh://src.fovea.cc/svn/flobopuyo/branches/autotools@1672 053fdb51-1f17-0410-be66-d00040120cd6

---
+++ gametools/CompositeDrawContext.cpp
@@ -0,0 +1,233 @@
+#include "CompositeDrawContext.h"
+
+/**
+ * CompositeSurface implementation
+ */
+
+CompositeSurface::CompositeSurface(IosSurface *baseSurface)
+    : m_baseSurface(baseSurface), m_isCropped(false)
+{
+    w = baseSurface->w;
+    h = baseSurface->h;
+}
+
+CompositeSurface::CompositeSurface(IosSurface *baseSurface,
+                                   const IosRect &cropRect)
+    : m_baseSurface(baseSurface), m_isCropped(true), m_cropRect(cropRect)
+{
+    w = cropRect.w;
+    h = cropRect.h;
+}
+
+CompositeSurface::~CompositeSurface()
+{
+    delete m_baseSurface;
+}
+
+// IosSurface methods
+bool CompositeSurface::isOpaque() const
+{
+    return m_baseSurface->isOpaque();
+}
+
+bool CompositeSurface::haveAbility(int ability) const
+{
+    return m_baseSurface->haveAbility(ability);
+}
+
+void CompositeSurface::dropAbility(int ability)
+{
+    if (!m_isCropped)
+        m_baseSurface->dropAbility(ability);
+}
+
+RGBA CompositeSurface::readRGBA(int x, int y)
+{
+    if (!m_isCropped)
+        return m_baseSurface->readRGBA(x, y);
+    return m_baseSurface->readRGBA(x+m_cropRect.x, y+m_cropRect.y);
+}
+
+IosSurface *CompositeSurface::shiftHue(float hue_offset, IosSurface *mask)
+{
+    return new CompositeSurface(m_baseSurface->shiftHue(hue_offset, mask));
+}
+
+IosSurface *CompositeSurface::shiftHSV(float h, float s, float v)
+{
+    return new CompositeSurface(m_baseSurface->shiftHSV(h, s, v));
+}
+
+IosSurface *CompositeSurface::setValue(float value)
+{
+    return new CompositeSurface(m_baseSurface->setValue(value));
+}
+
+IosSurface * CompositeSurface::resizeAlpha(int width, int height)
+{
+    return new CompositeSurface(m_baseSurface->resizeAlpha(width, height));
+}
+
+IosSurface * CompositeSurface::mirrorH()
+{
+    return new CompositeSurface(m_baseSurface->mirrorH());
+}
+
+void CompositeSurface::convertToGray()
+{
+    m_baseSurface->convertToGray();
+}
+
+// DrawTarget implementation
+void CompositeSurface::setClipRect(IosRect *rect)
+{
+    m_baseSurface->setClipRect(rect);
+}
+
+void CompositeSurface::setBlendMode(ImageBlendMode mode)
+{
+    //if (!m_isComposite)
+    m_baseSurface->setBlendMode(mode);
+}
+
+void CompositeSurface::draw(IosSurface *surf, IosRect *srcRect, IosRect *dstRect)
+{
+    CompositeSurface *s = static_cast<CompositeSurface *>(surf);
+    m_baseSurface->draw(s->m_baseSurface, srcRect, dstRect);
+}
+
+void CompositeSurface::drawHFlipped(IosSurface *surf, IosRect *srcRect, IosRect *dstRect)
+{
+    CompositeSurface *s = static_cast<CompositeSurface *>(surf);
+    m_baseSurface->drawHFlipped(s->m_baseSurface, srcRect, dstRect);
+}
+
+void CompositeSurface::drawRotatedCentered(IosSurface *surf, int angle, int x, int y)
+{
+    CompositeSurface *s = static_cast<CompositeSurface *>(surf);
+    m_baseSurface->drawRotatedCentered(s->m_baseSurface, angle, x, y);
+}
+
+void CompositeSurface::fillRect(const IosRect *rect, const RGBA &color)
+{
+    m_baseSurface->fillRect(rect, color);
+}
+
+void CompositeSurface::putString(IosFont *font, int x, int y, const char *text)
+{
+    m_baseSurface->putString(font, x, y, text);
+}
+
+/**
+ * CompositeImageLibrary implementation
+ */
+
+CompositeImageLibrary::CompositeImageLibrary(CompositeDrawContext &owner)
+    : m_owner(owner), m_baseDrawContext(owner.getBaseDrawContext()),
+      m_baseImageLibrary(m_baseDrawContext.getImageLibrary())
+{
+}
+
+IosSurface * CompositeImageLibrary::createImage(ImageType type, int w, int h, ImageSpecialAbility specialAbility)
+{
+    return new CompositeSurface(m_baseImageLibrary.createImage(type, w, h, specialAbility));
+}
+
+IosSurface * CompositeImageLibrary::loadImage(ImageType type, const char *path, ImageSpecialAbility specialAbility)
+{
+    CompositeSurfaceDefinition *def = m_owner.getCompositeSurfaceDefinition(path);
+    if (def == NULL)
+        return new CompositeSurface(m_baseImageLibrary.loadImage(type, path, specialAbility));
+    IosSurface *baseSurface = m_baseImageLibrary.loadImage(type, def->getPath(), specialAbility);
+    return new CompositeSurface(baseSurface, def->getCropRect());
+}
+
+IosFont * CompositeImageLibrary::createFont(const char *path, int size, IosFontFx fx)
+{
+    return m_baseImageLibrary.createFont(path, size, fx);
+}
+
+/**
+ * CompositeDrawContext implementation
+ */
+
+CompositeDrawContext::CompositeDrawContext(DrawContext *baseDrawContext)
+    : m_baseDrawContext(baseDrawContext), m_imageLibrary(*this)
+{
+    w = baseDrawContext->w;
+    h = baseDrawContext->h;
+}
+
+// DrawTarget implementation
+
+void CompositeDrawContext::setClipRect(IosRect *rect)
+{
+    m_baseDrawContext->setClipRect(rect);
+}
+
+void CompositeDrawContext::setBlendMode(ImageBlendMode mode)
+{
+    m_baseDrawContext->setBlendMode(mode);
+}
+
+void CompositeDrawContext::draw(IosSurface *surf, IosRect *srcRect, IosRect *dstRect)
+{
+    CompositeSurface *s = static_cast<CompositeSurface *>(surf);
+    if (! s->m_isCropped) {
+        m_baseDrawContext->draw(s->m_baseSurface, srcRect, dstRect);
+        return;
+    }
+    if (srcRect == NULL) {
+        m_baseDrawContext->draw(s->m_baseSurface, &(s->m_cropRect), dstRect);
+        return;
+    }
+}
+
+void CompositeDrawContext::drawHFlipped(IosSurface *surf, IosRect *srcRect, IosRect *dstRect)
+{
+    CompositeSurface *s = static_cast<CompositeSurface *>(surf);
+    m_baseDrawContext->drawHFlipped(s->m_baseSurface, srcRect, dstRect);
+}
+
+void CompositeDrawContext::drawRotatedCentered(IosSurface *surf, int angle, int x, int y)
+{
+    CompositeSurface *s = static_cast<CompositeSurface *>(surf);
+    m_baseDrawContext->drawRotatedCentered(s->m_baseSurface, angle, x, y);
+}
+
+void CompositeDrawContext::fillRect(const IosRect *rect, const RGBA &color)
+{
+    m_baseDrawContext->fillRect(rect, color);
+}
+
+void CompositeDrawContext::putString(IosFont *font, int x, int y, const char *text)
+{
+    m_baseDrawContext->putString(font, x, y, text);
+}
+
+// Specific methods
+
+void CompositeDrawContext::declareCompositeSurface(const char *key,
+                                                   const char *path,
+                                                   IosRect &cropRect)
+{
+    m_compositeSurfaceDefs[key] = CompositeSurfaceDefinition(path, cropRect);
+}
+
+void CompositeDrawContext::declareCompositeSurface(const char *key,
+                                                   const char *path,
+                                                   int x, int y, int w, int h)
+{
+    IosRect cropRect = {x, y, w, h};
+    declareCompositeSurface(key, path, cropRect);
+}
+
+CompositeSurfaceDefinition *CompositeDrawContext::getCompositeSurfaceDefinition(const char *key)
+{
+    std::map<std::string, CompositeSurfaceDefinition>::iterator iter;
+    iter = m_compositeSurfaceDefs.find(key);
+    if (iter == m_compositeSurfaceDefs.end())
+        return NULL;
+    return &(iter->second);
+}
+
---
+++ gametools/CompositeDrawContext.h
@@ -0,0 +1,108 @@
+#ifndef _COMPOSITEDRAWCONTEXT_H_
+#define _COMPOSITEDRAWCONTEXT_H_
+
+#include <string>
+#include <map>
+#include "drawcontext.h"
+
+class CompositeDrawContext;
+
+class CompositeSurfaceDefinition
+{
+public:
+    CompositeSurfaceDefinition() {}
+    CompositeSurfaceDefinition(const char *path, const IosRect &cropRect)
+        : m_path(path), m_cropRect(cropRect) {}
+    const char *getPath() const { return m_path.data(); }
+    const IosRect &getCropRect() const { return m_cropRect; }
+private:
+    std::string m_path;
+    IosRect m_cropRect;
+};
+
+class CompositeSurface : public IosSurface
+{
+public:
+    CompositeSurface(IosSurface *baseSurface);
+    CompositeSurface(IosSurface *baseSurface, const IosRect &cropRect);
+    virtual ~CompositeSurface();
+    // IosSurface methods
+    virtual bool isOpaque() const;
+	virtual bool haveAbility(int ability) const;
+	virtual void dropAbility(int ability);
+    virtual RGBA readRGBA(int x, int y);
+    virtual IosSurface *shiftHue(float hue_offset, IosSurface *mask = NULL);
+    virtual IosSurface *shiftHSV(float h, float s, float v);
+    virtual IosSurface *setValue(float value);
+    virtual IosSurface * resizeAlpha(int width, int height);
+    virtual IosSurface * mirrorH();
+    virtual void         convertToGray();
+    // DrawTarget methods
+    virtual void setClipRect(IosRect *rect);
+    virtual void setBlendMode(ImageBlendMode mode);
+    virtual void draw(IosSurface *surf, IosRect *srcRect, IosRect *dstRect);
+    virtual void drawHFlipped(IosSurface *surf, IosRect *srcRect, IosRect *dstRect);
+    virtual void drawRotatedCentered(IosSurface *surf, int angle, int x, int y);
+	virtual void fillRect(const IosRect *rect, const RGBA &color);
+    virtual void putString(IosFont *font, int x, int y, const char *text);
+private:
+    IosSurface *m_baseSurface;
+    bool m_isCropped;
+    IosRect m_cropRect;
+    friend class CompositeDrawContext;
+};
+
+class CompositeImageLibrary : public ImageLibrary
+{
+public:
+    CompositeImageLibrary(CompositeDrawContext &owner);
+    virtual IosSurface * createImage(ImageType type, int w, int h, ImageSpecialAbility specialAbility = 0);
+    virtual IosSurface * loadImage(ImageType type, const char *path, ImageSpecialAbility specialAbility = 0);
+    virtual IosFont    * createFont(const char *path, int size, IosFontFx fx = Font_STD);
+private:
+    CompositeDrawContext &m_owner;
+    DrawContext  &m_baseDrawContext;
+    ImageLibrary &m_baseImageLibrary;
+};
+
+class CompositeDrawContext : public DrawContext
+{
+public:
+    CompositeDrawContext(DrawContext *baseDrawContext);
+    virtual void flip() {
+        m_baseDrawContext->flip();
+    }
+    virtual int getHeight() const {
+        return m_baseDrawContext->getHeight();
+    }
+    virtual int getWidth() const {
+        return m_baseDrawContext->getWidth();
+    }
+    virtual ImageLibrary &getImageLibrary() {
+        return m_imageLibrary;
+    }
+    // DrawTarget implementation
+    virtual void setClipRect(IosRect *rect);
+    virtual void setBlendMode(ImageBlendMode mode);
+    virtual void draw(IosSurface *surf, IosRect *srcRect, IosRect *dstRect);
+    virtual void drawHFlipped(IosSurface *surf, IosRect *srcRect, IosRect *dstRect);
+    virtual void drawRotatedCentered(IosSurface *surf, int angle, int x, int y);
+	virtual void fillRect(const IosRect *rect, const RGBA &color);
+    virtual void putString(IosFont *font, int x, int y, const char *text);
+    // Specific methods
+    DrawContext &getBaseDrawContext() const { return *m_baseDrawContext; }
+    void declareCompositeSurface(const char *key,
+                                 const char *path,
+                                 IosRect &cropRect);
+    void declareCompositeSurface(const char *key,
+                                 const char *path,
+                                 int x, int y, int w, int h);
+    CompositeSurfaceDefinition *getCompositeSurfaceDefinition(const char *key);
+private:
+    DrawContext *m_baseDrawContext;
+    CompositeImageLibrary m_imageLibrary;
+    std::map<std::string, CompositeSurfaceDefinition> m_compositeSurfaceDefs;
+};
+
+#endif // _COMPOSITEDRAWCONTEXT_H_
+
