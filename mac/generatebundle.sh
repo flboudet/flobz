9dbab5204a1f373123de4948a5c5f42cf9eb74b5
flobo <flboudet@gmail.com>
2011-02-02 20:48:13 +0000

    Mise a jour du nom de l'application
    git-svn-id: svn+ssh://src.fovea.cc/svn/flobopuyo/branches/autotools@1769 053fdb51-1f17-0410-be66-d00040120cd6

--- mac/generatebundle.sh
+++ mac/generatebundle.sh
@@ -35,4 +35,8 @@ ${SOURCE_DIRECTORY}/configure -disable-dependency-tracking CXXFLAGS="${ARCHFLAGS
 make -j3
 make -j3 mac-bundle
 
+# Additional steps to compensate bad scripting
+rm -rf FloboPop.app/Contents/Resources/data/complete.002
+codesign -f -v -s "3rd Party Mac Developer Application: Fovea" "FloboPop.app"
+
 #configure -disable-dependency-tracking SDL_LIBS="-F/Users/flobo/Library/Frameworks/ -framework Cocoa -framework SDL -framework SDL_image -framework SDL_ttf -framework SDL_mixer" SDL_CFLAGS="-I/Users/flobo/Library/Frameworks/SDL.framework/Headers/ -F/Users/flobo/Library/Frameworks/ -framework SDL -framework SDL_image -framework SDL_ttf -framework SDL_mixer" SDL_VERSION="1.2.12" CXXFLAGS="-arch i386 -arch ppc" CFLAGS="-arch i386 -arch ppc" OBJCFLAGS="-arch i386 -arch ppc"
