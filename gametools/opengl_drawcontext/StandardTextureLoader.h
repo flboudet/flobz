46ea12d6ad326fc1855df7d64713f510f35a17a7
flobo <flboudet@gmail.com>
2011-12-10 12:13:23 +0000

    added forgotten StandardTextureLoader sources
    git-svn-id: svn+ssh://src.fovea.cc/svn/flobopuyo/branches/autotools@1884 053fdb51-1f17-0410-be66-d00040120cd6

---
+++ gametools/opengl_drawcontext/StandardTextureLoader.h
@@ -0,0 +1,22 @@
+#ifndef _STANDARD_TEXTURE_LOADER_H
+#define _STANDARD_TEXTURE_LOADER_H
+
+#include "opengl_drawcontext/OpenGLDrawContext.h"
+#include "DataPathManager.h"
+
+class StandardTextureLoader : public OpenGLBackendUtil
+{
+public:
+    StandardTextureLoader(DataPathManager *dataPathManager);
+    virtual unsigned short * utf8ToUnicode(const char *utf8Text, unsigned short *unicodeTextBuffer, size_t unicodeTextBufferSize);
+    virtual OpenGLRawImage * loadImage(ImageType type, const char *path);
+    virtual DataPathManager * getdataPathManager() const { return m_dataPathManager; }
+private:
+    OpenGLRawImage * loadImagePNG(ImageType type, const char *path);
+    OpenGLRawImage * loadImageJPG(ImageType type, const char *path);
+private:
+    DataPathManager *m_dataPathManager;
+};
+
+#endif // _STANDARD_TEXTURE_LOADER_H
+
