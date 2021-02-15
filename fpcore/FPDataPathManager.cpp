a39c9858be33cdde2b083bc2d840899521db38f1
Florent Boudet <florent.boudet@cassidian.com>
2021-02-15 23:11:23 +0100

    FPDataPathManager: getPath() now returns an empty string on non-existing files (instead of exception)

--- fpcore/FPDataPathManager.cpp
+++ fpcore/FPDataPathManager.cpp
@@ -61,7 +61,6 @@ FPDataPackage::FPDataPackage(FPDataPathManager *owner,
 
 std::string FPDataPackage::getPath(const char *shortPath) const
 {
-    std::cout << "DataPackage Get path " << shortPath << std::endl;
     String ret = (m_owner->getPathInPack(shortPath, m_packageNumber));
     if (ret != "")
         return (const char *) ret;
@@ -143,15 +142,12 @@ bool FPDataPathManager::hasFile(String shortPath) const
 
 String FPDataPathManager::getPath(String shortPath) const
 {
-    std::cout << "DataPathMgr Get path " << shortPath << std::endl;
     for (int i = 0 ; i < m_dataPaths.size() ; i++) {
         FilePath testPath(m_dataPaths[i].combine(shortPath));
         if (testPath.exists())
             return testPath.getPathString();
     }
-    std::cout << "DataPathMgr file not found" << std::endl;
-    return "";
-    //throw Exception(String("File ") + shortPath + " not found !");
+    return ""; // File not found
 }
 
 String FPDataPathManager::getPathInPack(String shortPath, int packPathIndex) const
